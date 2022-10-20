1ac5e222f3aced0f866c0f68a8940c5e7de522bc
Reinaldo <sora2854@gmail.com>
2022-10-19 23:31:08 -0400

    created array with objects for each project and their properties

---
+++ src/projects.js
@@ -0,0 +1,68 @@
+import dom from './dom';
+
+const projects = (() => {
+  let projectsList = [];
+
+  // GET DEFAULT PROJECTS AND TASKS FROM LOCAL STORAGE
+  if (localStorage.getItem('projects') === null) {
+    projectsList = [
+      {
+        icon: 'fa-tools',
+        title: 'Craft New Project',
+        tasks: [
+          {
+            title: 'The Big Test',
+            description: 'Test descrption. 2008, the year where all assets dropped to the floor :(',
+            date: '2008-08-08',
+            priority: 'low',
+            projectIndex: 0,
+            taskIndex: 0,
+            completed: false
+          }
+        ]
+      },
+    ];
+  }
+
+  else {
+    const projectsFromStorage = JSON.parse(localStorage.getItem('projects'));
+    projectsList = projectsFromStorage;
+  }
+
+  class Project {
+    constructor(icon, title) {
+      this.icon = icon;
+      this.title = title;
+      this.tasks = [];
+    }
+  }
+
+  function addProject(icon, title) {
+    const project = new Project(icon, title);
+    projectsList.push(project);
+    dom.showProjects();
+  }
+
+  function editProject(icon, title, index, link) {
+    projectsList[index].icon = icon;
+    projectsList[index].title = title;
+    dom.showProjects();
+    dom.selectLink(link, index, 'edit');
+  }
+
+  function deleteProject(index) {
+    if (index > -1) {
+      projectsList.splice(index, 1);
+    }
+    dom.showProjects();
+  }
+
+  return {
+    projectsList,
+    addProject,
+    editProject,
+    deleteProject,
+  };
+})();
+
+export default projects;
